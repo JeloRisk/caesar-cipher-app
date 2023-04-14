6213ed9ad690e961dd7e475817145fac336e5206
JeloRisk <mamuadjeromemarlo@gmail.com>
2023-04-14 22:36:09 +0800

    add readfeature

--- app/src/main/java/com/example/alohomora/GuessingGame.java
+++ app/src/main/java/com/example/alohomora/GuessingGame.java
@@ -1,5 +1,9 @@
 package com.example.alohomora;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,6 +12,10 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Random;
 
 public class GuessingGame extends AppCompatActivity {
@@ -16,6 +24,9 @@ public class GuessingGame extends AppCompatActivity {
     private EditText shiftEditText;
     private TextView resultTextView;
     private RadioGroup radioGroup;
+
+    private static final int READ_REQUEST_CODE = 42;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +38,7 @@ public class GuessingGame extends AppCompatActivity {
         resultTextView = findViewById(R.id.result_text_view);
         radioGroup = findViewById(R.id.radio_group);
 
+        Button button =findViewById(R.id.select_file_button);
         Button convertButton = findViewById(R.id.convert_button);
         convertButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,7 +56,52 @@ public class GuessingGame extends AppCompatActivity {
                 resultTextView.setText(result);
             }
         });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performFileSearch();
+            }
+        });
     }
+    private void performFileSearch() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("text/plain");
+        startActivityForResult(intent, READ_REQUEST_CODE);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        super.onActivityResult(requestCode, resultCode, resultData);
+
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            if (resultData != null) {
+                Uri uri = resultData.getData();
+                try {
+                    String content = readTextFromUri(uri);
+                    resultTextView.setText(content);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private String readTextFromUri(Uri uri) throws IOException {
+        InputStream inputStream = getContentResolver().openInputStream(uri);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            stringBuilder.append(line);
+            stringBuilder.append("\n");
+        }
+        inputStream.close();
+        return stringBuilder.toString();
+    }
+
+
 
     // Encrypts the given message using Caesar cipher and the given shift
     private String encrypt(String message, int shift) {
